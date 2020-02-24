a8ce12c96bcdf90c490d2fd1e49321577bdf2a96
Bikash Thapa <bikashthapa.np@gmail.com>
2020-02-24 13:32:34 -0500

    initial setup/server set up

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// load express
+const express = require('express') // v14.6.4
+const path = require('path') // node module
+
+const app = express() // initialize the express app
+
+// calls from environment variable or port 3000
+const port = process.env.PORT || 3000 // enable the port address
+
+// main folder/source to render
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// use express static middleware to setup the server
+app.use(express.static(publicDirectoryPath))
+
+// listening to the port
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
